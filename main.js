c1f5591350518629922b8e827eea6ba74a0f03e0
NasouhAlsamman <nasouhe@gmail.com>
2023-01-30 00:00:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let img=document.querySelector('.img');
+let container=document.querySelector('.container');
+
+function change(num){
+    img.src = num;
+}
+
+function back(color){
+container.style.background = color;
+}
